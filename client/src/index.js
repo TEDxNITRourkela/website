b613435708c1860c6d3a43b8f0c8c3fd046127c5
Ritesh Patil <riteshsp2000@gmail.com>
2020-11-25 21:42:43 +0530

    Setup create-react-app

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import App from './views/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
