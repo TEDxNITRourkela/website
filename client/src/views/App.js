c4f16183fd3f08b56254cdf287400ad489d2a00e
Ritesh Patil <riteshsp2000@gmail.com>
2020-12-29 16:05:26 +0530

    Add Dates + content + logo

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import svg from '../assets/img/logo.svg';
-import leftCenter from '../assets/img/leftCenter.svg';
-import rightCenter from '../assets/img/rightCenter.svg';
-import leftTriangle from '../assets/img/leftTriangle.svg';
-import rightTriangle from '../assets/img/rightTriangle.svg';
+import horizontalRectange from '../assets/img/v1/horizontal_rectangle.svg';
+import logo from '../assets/img/logo.svg';
 
 function App() {
   const classes = useStyles();
@@ -13,84 +10,19 @@ function App() {
   return (
     <div className={classes.screen}>
       <div className={classes.container}>
-        <img src={svg} alt='logo' className={classes.logo} />
+        <h2 className={classes.content}>TECHNOLOGY . ENTERTAINMENT . DESIGN</h2>
 
-        <div className={classes.content}>
-          <h3 className={classes.text}>
-            TEDxNITRourkela is inducting for the Tech, Design and Management
-            Teams. We need capable and confident individuals who can deliver the
-            best, to make this event fruitful one.{' '}
-          </h3>
+        <img className={classes.logo} src={logo} alt='Logo' />
 
-          <h3 className={classes.textBold}>
-            If you think you got what it takes, fill out the given form.
-          </h3>
-
-          <h3 className={classes.text}>
-            The form will remain open till{' '}
-            <span className={classes.textBold}>30th Nov, 2020 11:59 PM</span>{' '}
-            and there will be no late submissions. <br /> If you have any
-            queries whatsoever feel free to mail at:{' '}
-            <a
-              className={classes.textBold}
-              href='mailto:contact-us@tedxnitrourkela.com'
-              style={{ '&:hover': { textDecoration: 'underline' } }}
-            >
-              contact-us@tedxnitrourkela.com
-            </a>
-          </h3>
-
-          {/* <h3 className={classes.text}>
-            *(unless it's due to unavoidable circumstances)
-          </h3> */}
-        </div>
-
-        <div className={classes.buttonsContainer}>
-          <h3 className={classes.textBold}>
-            Click on the team that you want to apply
-          </h3>
-
-          <div className={classes.buttons}>
-            <a
-              target='_blank'
-              href='https://bit.ly/TEDxNITRclub-tech-inductions'
-              rel='noopener noreferrer'
-            >
-              <button className={classes.button}>Technical</button>
-            </a>
-            <a
-              target='_blank'
-              href='https://bit.ly/TEDxNITRclub-design-inductions'
-              rel='noopener noreferrer'
-            >
-              <button className={classes.button}>Design</button>
-            </a>
-            <a
-              target='_blank'
-              href='https://bit.ly/TEDxNITRclub-management-inductions'
-              rel='noopener noreferrer'
-            >
-              <button className={classes.button}>Management</button>
-            </a>
-          </div>
-        </div>
+        <h2 className={classes.date}>13th March 2021</h2>
       </div>
-      <img src={leftCenter} alt='left center' className={classes.leftCenter} />
-      <img
-        src={rightCenter}
-        alt='right center'
-        className={classes.rightCenter}
-      />
-      <img
-        src={rightTriangle}
-        alt='right center'
-        className={classes.rightTriangle}
-      />
-      <img
-        src={leftTriangle}
-        alt='right center'
-        className={classes.leftTriangle}
-      />
+
+      <a
+        className={classes.contactUs}
+        href='mailto:contact-us@tedxnitrourkela.com'
+      >
+        Contact Us
+      </a>
     </div>
   );
 }
@@ -116,23 +48,37 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 150,
   },
   logo: {
-    marginBottom: 80,
-    marginTop: 50,
-    width: '40%',
+    marginBottom: 30,
+    marginTop: 20,
+    width: '82%',
     [theme.breakpoints.down('xs')]: {
-      width: '65%',
+      width: '75%',
     },
   },
   content: {
-    width: '70%',
-    display: 'flew',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
+    textAlign: 'center',
+    color: 'rgba(255,255,255, 0.5)',
+    fontWeight: 'bold',
+    fontSize: 14,
+    fontFamily: 'Zilla Slab, serif',
+    letterSpacing: 3,
     [theme.breakpoints.down('xs')]: {
-      width: '85%',
+      fontSize: 14,
+    },
+  },
+  date: {
+    width: '100%',
+    textAlign: 'center',
+    color: 'rgba(255,255,255, 0.5)',
+    fontWeight: 'bold',
+    fontSize: 32,
+    fontFamily: 'Zilla Slab, serif',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 20,
     },
   },
   text: {
@@ -146,101 +92,14 @@ const useStyles = makeStyles((theme) => ({
       fontSize: 14,
     },
   },
-  textBold: {
-    fontSize: 18,
-    textAlign: 'center',
-    fontFamily: 'Helvetica',
-    fontWeight: '700',
-    marginTop: 20,
+  contactUs: {
+    position: 'absolute',
+    bottom: 80,
+    textDecoration: 'underline',
     color: '#fff',
+    fontSize: 24,
     [theme.breakpoints.down('xs')]: {
-      fontSize: 14,
+      fontSize: 20,
     },
-  },
-  buttonsContainer: {
-    marginTop: 50,
-    marginBottom: 30,
-  },
-  buttons: {
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column',
-      alignItems: 'space-around',
-      justifyContent: 'center',
-      marginTop: 40,
-    },
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  button: {
-    margin: 20,
-    padding: 10,
-    backgroundColor: '#000000',
-    color: '#fff',
-    borderRadius: 7,
-    border: '1px solid #FF2B06',
-    width: 120,
-    fontSize: 16,
-    textAlign: 'center',
-    fontFamily: 'Helvetica',
-    fontWeight: '600',
-    '&:hover': {
-      backgroundColor: '#FF2B06',
-      color: '#fff',
-    },
-    [theme.breakpoints.down('xs')]: {
-      margin: 10,
-      padding: 10,
-    },
-  },
-  leftCenter: {
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-    right: 0,
-    top: 0,
-    margin: 0,
-    padding: 0,
-  },
-  rightCenter: {
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-    left: 0,
-    top: 0,
-    margin: 0,
-    padding: 0,
-  },
-  leftTriangle: {
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-    left: 0,
-    top: 0,
-    margin: 0,
-    padding: 0,
-    marginLeft: '-12%',
-  },
-  rightTriangle: {
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-    right: 0,
-    top: 0,
-    margin: 0,
-    padding: 0,
-    marginRight: '-12%',
   },
 }));
