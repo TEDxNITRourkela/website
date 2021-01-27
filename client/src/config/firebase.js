8742920399a68c2fb9ac7c0cdfb281fb347e41ea
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-01-27 15:55:22 +0530

    Apply suggestions from code review

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -2,14 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const configureFirebase = async () => {
-  if (process.env.NODE_ENV === 'production') {
-    const response = await fetch('/__/firebase/init.json');
-    const config = await response.json();
-    firebase.initializeApp(config);
-  } else {
-    import('./keys').then(({ firebaseDevConfig }) =>
-      firebase.initializeApp(firebaseDevConfig),
-    );
-  }
+  const response = await fetch('/__/firebase/init.json');
+  const config = await response.json();
+  firebase.initializeApp(config);
 };
 export default configureFirebase;
