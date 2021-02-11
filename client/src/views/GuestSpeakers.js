1758b3677087f0503d97ef66a2da39eea6358d65
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-12 00:59:57 +0530

    Minor changes

--- client/src/views/GuestSpeakers.js
+++ client/src/views/GuestSpeakers.js
@@ -7,26 +7,32 @@ import GuestCard from '../components/speakers/GuestCard';
 
 // Assets
 import { GUESTS } from '../assets/placeholder/speakers';
+import topLeft from '../assets/img/homepage/topLeft2.png';
 
 function About() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.root}>
-      <h1>guest speaker page</h1>
+    <>
+      <Container className={classes.root}>
+        <h1 className={classes.title}>Guest Speakers</h1>
 
-      <div className={classes.guestsContainer}>
-        {GUESTS.map(({ name, description, shortDescription, image, links }) => (
-          <GuestCard
-            name={name}
-            description={description}
-            shortDescription={shortDescription}
-            image={image}
-            links={links}
-          />
-        ))}
-      </div>
-    </Container>
+        <div className={classes.guestsContainer}>
+          {GUESTS.map(
+            ({ name, description, shortDescription, image, links }) => (
+              <GuestCard
+                name={name}
+                description={description}
+                shortDescription={shortDescription}
+                image={image}
+                links={links}
+              />
+            ),
+          )}
+        </div>
+      </Container>
+      <img src={topLeft} alt='top left' className={classes.topLeft} />
+    </>
   );
 }
 
@@ -38,6 +44,8 @@ const useStyles = makeStyles(() => ({
     height: 'calc(100% - 100px)',
     minHeight: '100vh',
     marginTop: '100px',
+    paddingTop: '50px',
+    position: 'relative',
   },
   guestsContainer: {
     width: '100%',
@@ -46,5 +54,19 @@ const useStyles = makeStyles(() => ({
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    width: '100%',
+    textAlign: 'center',
+    font: 'Zilla Slab',
+    weight: '700',
+    fontSize: '36px',
+    lineHeight: '43px',
+    color: '#ffffff',
+  },
+  topLeft: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
   },
 }));
