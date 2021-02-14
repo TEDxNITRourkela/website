c2ad8b843692f15fe3ad71f266713d3cec9276d3
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-14 13:17:02 +0530

    Minor fixes

--- client/src/index.js
+++ client/src/index.js
@@ -4,27 +4,20 @@ import ReactDOM from 'react-dom';
 import App from './views/App';
 import Root from './Root';
 
-// import configureFirebase from './config/firebase';
+import configureFirebase from './config/firebase';
 
 import('./index.css');
 
-// configureFirebase()
-//   .then(() => {
-//     ReactDOM.render(
-//       <Root>
-//         <App />
-//       </Root>,
-//       document.querySelector('#root'),
-//     );
-//   })
-//   .catch((error) => {
-//     // eslint-disable-next-line
-//     console.log('Firebase Initialization Error: ', error);
-//   });
-
-ReactDOM.render(
-  <Root>
-    <App />
-  </Root>,
-  document.querySelector('#root'),
-);
+configureFirebase()
+  .then(() => {
+    ReactDOM.render(
+      <Root>
+        <App />
+      </Root>,
+      document.querySelector('#root'),
+    );
+  })
+  .catch((error) => {
+    // eslint-disable-next-line
+    console.log('Firebase Initialization Error: ', error);
+  });
