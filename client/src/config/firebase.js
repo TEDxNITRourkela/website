7a42bd16e0ee617caae74577ea11329bfcd344b3
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-17 21:19:34 +0530

    Change firebase initialization

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,24 +3,27 @@ import 'firebase/firestore';
 import 'firebase/analytics';
 
 const configureFirebase = async () => {
-  if (process.env.NODE_ENV === 'production') {
-    const response = await fetch('/__/firebase/init.json');
-    const config = await response.json();
-    firebase.initializeApp(config);
-  } else {
-    const firebaseConfig = {
-      apiKey: process.env.APIKEY,
-      authDomain: process.env.AUTHDOMAIN,
-      databaseURL: process.env.DATABASEURL,
-      projectId: process.env.PROJECTID,
-      storageBucket: process.env.STORAGEBUCKET,
-      messagingSenderId: process.env.MESSAGINGSENDERID,
-      appId: process.env.APPID,
-      measurementId: process.env.MEASUREMENTID,
-    };
+  const response = await fetch('/__/firebase/init.json');
+  const config = await response.json();
+  firebase.initializeApp(config);
+  // if (process.env.NODE_ENV === 'production') {
+  //   const response = await fetch('/__/firebase/init.json');
+  //   const config = await response.json();
+  //   firebase.initializeApp(config);
+  // } else {
+  //   const firebaseConfig = {
+  //     apiKey: process.env.APIKEY,
+  //     authDomain: process.env.AUTHDOMAIN,
+  //     databaseURL: process.env.DATABASEURL,
+  //     projectId: process.env.PROJECTID,
+  //     storageBucket: process.env.STORAGEBUCKET,
+  //     messagingSenderId: process.env.MESSAGINGSENDERID,
+  //     appId: process.env.APPID,
+  //     measurementId: process.env.MEASUREMENTID,
+  //   };
 
-    firebase.initializeApp(firebaseConfig);
-  }
+  //   firebase.initializeApp(firebaseConfig);
+  // }
 };
 
 export const { firestore, analytics } = firebase;
