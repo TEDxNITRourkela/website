b66b29edad2d395de86d7dc58a620dd3fe4f5cdb
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-20 19:24:15 +0530

    Shift cloudinary links from versioned to direct

--- client/src/assets/img/graphics.js
+++ client/src/assets/img/graphics.js
@@ -2,17 +2,17 @@
 
 export const GRAPHICS = Object.freeze({
   TOP_LEFT:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/topLeft_eik5q8.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/topLeft_eik5q8.png',
   TOP_RIGHT:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/topRight_fxtfp8.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/topRight_fxtfp8.png',
   BOTTOM_LEFT:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/bottomLeft_fcjrjy.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/bottomLeft_fcjrjy.png',
   BOTTOM_RIGHT:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/bottomRight_zdyj18.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/bottomRight_zdyj18.png',
   TOP_LEFT_POLYGON:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/topLeftPolygon_leozkc.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/topLeftPolygon_leozkc.png',
   TOP_LEFT_ELLIPSE:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/topLeftEllipse_xnp0mb.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/topLeftEllipse_xnp0mb.png',
   CIRCLES:
-    'https://res.cloudinary.com/tedxnitrourkela/image/upload/v1613307501/assets/graphics/circles_fjjqwx.png',
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/graphics/circles_fjjqwx.png',
 });
