7a4e962419fee86a2eb1a097fd249ac06975b351
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-21 15:40:44 +0530

    Add incentives table on home page

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react';
 
 // Libraries
 import { Helmet } from 'react-helmet';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Container } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 // Utilities
 import { analytics } from '../config/firebase';
@@ -14,6 +15,7 @@ import Subscribe from '../components/homepage/Subscription';
 import TedContent from '../components/homepage/TedContent';
 import Partners from '../components/homepage/Partners';
 import ContentBanner from '../components/shared/ContentBanner';
+import Incentives from '../components/participate/Incentives';
 
 import { CONTENT2 } from '../assets/placeholder/homepage';
 
@@ -36,6 +38,16 @@ function Home() {
       <Index />
       <ContentBanner DATA={CONTENT2.TWO} smallLogo wrapContainer />
       <ContentBanner DATA={CONTENT2.ONE} wrapContainer />
+      <Container className={classes.container}>
+        <Incentives title='Campus Representative Program' />
+
+        <Link to='/campus_ambassadors' className={classes.link}>
+          <button type='button' className={classes.button}>
+            Know More
+          </button>
+        </Link>
+      </Container>
+
       {/* <Subscribe /> */}
       {/* <Partners /> */}
     </>
@@ -51,5 +63,27 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.default,
     marginTop: '100px',
     position: 'relative',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  button: {
+    marginTop: 50,
+    border: '1px solid #FF2B06',
+    borderRadius: '6px',
+    backgroundColor: '#1a1a1a',
+    padding: 'auto 20px',
+    color: '#ffffff',
+    minWidth: '150px',
+    minHeight: '30px',
+    width: 'auto',
+    fontSize: '16px',
+    '&:hover': {
+      backgroundColor: '#FF2B06',
+      cursor: 'pointer',
+    },
   },
 }));
