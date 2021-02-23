5896dd967f95d50b88dcee1c87fcbd611911ddfe
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-23 22:01:37 +0530

    Update ticket image for referrals

--- client/src/assets/img/pages.js
+++ client/src/assets/img/pages.js
@@ -52,4 +52,6 @@ export const PARTNERS = Object.freeze({
 export const TICKETS = Object.freeze({
   TICKET:
     'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/tickets/Group_609_1_xx35tj.png',
+  DISCOUNTED:
+    'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/tickets/Group_609_qxldg3.png',
 });
