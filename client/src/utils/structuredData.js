d4b2cd0050474e9a647a9b46743bc8bafa7b3d81
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-26 00:44:19 +0530

    Update FAQs

--- client/src/utils/structuredData.js
+++ client/src/utils/structuredData.js
@@ -94,6 +94,24 @@ export const faqData = {
   mainEntity: [
     {
       '@type': 'Question',
+      name: 'What is TEDxNITRourkela?',
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text:
+          "TEDxNITRourkela is an independently organized TED event coordinated and curated by a group of NIT Rourkela students. We present people with the opportunity to suggest innovative and efficient ideas to tackle our society's various issues, thus gathering a diverse range of voices from many disciplines that exist to help humanity grow. From essential, radical, and exciting ideas to electrifying performances from performers who are on top of their respective fields, we aspire to deliver to you an experience you would not want to miss.",
+      },
+    },
+    {
+      '@type': 'Question',
+      name: 'When is TEDxNITRourkela being conducted?',
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text:
+          'The event is being conducted online on 13th and 14th of March 2021',
+      },
+    },
+    {
+      '@type': 'Question',
       name: 'Who are the speakers for TEDxNITRourkela?',
       acceptedAnswer: {
         '@type': 'Answer',
