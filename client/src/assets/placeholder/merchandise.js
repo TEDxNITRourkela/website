8b08e3976df7792f05910c5b26e1750dfd507db6
Ritesh Patil <riteshsp2000@gmail.com>
2021-02-25 18:50:41 +0530

    Minor changes and cleanup code

--- client/src/assets/placeholder/merchandise.js
+++ client/src/assets/placeholder/merchandise.js
@@ -5,33 +5,18 @@ const createData = (column1, column2, column3) => {
 
 export const MERCH = [
   {
-    name: 'THE CLASS-Y 2021 TEE',
+    name: 'The Class-X of 2021 ',
     imageUrl:
       'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/merchandise/WhatsApp_Image_2021-02-18_at_10.07_1_oxwb4r.png',
     description:
       'Celebrate your adventurous journey with this classy CLASS OF 2021 Tee, and cherish the wonderful memories.',
     price: '399',
-    paymentLink:
-      'https://studentactivitycenter.stores.instamojo.com/product/291815/the-class-y-2021-tee-tedxnitrourkela/',
-    dataID: '291815',
     tshirtCode: 'classof2021',
   },
   {
-    name: 'THE NITR @ 60 TEE',
+    name: 'The X-Treme Tee',
     imageUrl:
-      'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/merchandise/NITR_60_1_bkovwe.jpg',
-    description:
-      'Flaunt the SEXAGENARION legacy of NITR with this cool NITR@60 Black Tee.',
-    price: '349',
-    paymentLink:
-      'https://studentactivitycenter.stores.instamojo.com/product/291805/the-nitr-60-tee-tedxnitrourkela/',
-    dataID: '291805',
-    tshirtCode: 'nitr60',
-  },
-  {
-    name: 'THE X-TREME TEE',
-    imageUrl:
-      'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/merchandise/X_giombl.jpg',
+      'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/merchandise/X_factor_2_muxcy2.jpg',
     description:
       'Add a WOW factor to your wardrobe with this cool X-TREME Tee, and flaunt that fresh design!',
     price: '399',
@@ -39,6 +24,16 @@ export const MERCH = [
       'https://studentactivitycenter.stores.instamojo.com/product/291810/the-x-treme-tee-tedxnitrourkela/',
     dataID: '291810',
     tshirtCode: 'xtreme',
+  },
+  {
+    name: 'The NITR @ 60 Tee',
+    imageUrl:
+      'https://res.cloudinary.com/tedxnitrourkela/image/upload/assets/pages/merchandise/NITR_60_1_bkovwe.jpg',
+    description:
+      'Flaunt the SEXAGENARION legacy of NITR with this cool NITR@60 Black Tee.',
+    price: '349',
+
+    tshirtCode: 'nitr60',
   },
 ];
 
