44921c18f4fa1fd363133ae377419a105ec97156
Ritesh Patil <riteshsp2000@gmail.com>
2021-04-09 20:20:48 +0530

    Fix partners not showing up error

--- client/src/views/Partner.js
+++ client/src/views/Partner.js
@@ -1,117 +1,83 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
 
 // Libraries
-import { makeStyles, Container, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import { Container } from '@material-ui/core';
 
-// Assets
-import { GRAPHICS } from '../assets/img/graphics';
+// Utilities
+import { analytics } from '../config/firebase';
 
-function Partner({ DATA, home, main = false }) {
-  const classes = useStyles(main);
+// Component
+import Partners from '../components/partner/Partners';
+import ContentBanner from '../components/shared/ContentBanner';
 
-  const renderPartnerContainer = (
-    <div className={classes.wrapper}>
-      <div className={classes.titleContainer}>
-        <Typography variant='h2' className={classes.title}>
-          {home ? 'Partners' : DATA.title}
-        </Typography>
-        <div className={classes.horizontalLine} />
-      </div>
+// Placeholder
+import { PARTNER } from '../assets/placeholder/partner';
 
-      <div className={classes.partnerContainer}>
-        {DATA.partners.map((sponsor) => (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href={sponsor.href}
-            title={sponsor.hrefTitle}
-            key={sponsor.href}
-            className={classes.imgContainer}
-            style={{
-              width: `${sponsor.size}`,
-            }}
-          >
-            <img src={sponsor.img} alt='Sponsor' className={classes.sponsor} />
-          </a>
-        ))}
-      </div>
-
-      {home && (
-        <div className={classes.navigator}>
-          <Link to='/partners' style={{ color: '#ffffff' }}>
-            View all Partners
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+function Partner() {
+  useEffect(() => {
+    analytics().logEvent('Partner Page Loaded');
+  }, []);
 
   return (
-    <Container
-      style={{
-        marginTop: home ? '20px' : '120px',
-      }}
-    >
-      {!home && (
-        <img src={GRAPHICS.TOP_LEFT} alt='ellipses' className={classes.image} />
-      )}
-      {renderPartnerContainer}
-    </Container>
+    <>
+      <Helmet>
+        <title>TEDxNITRourkela | Partners</title>
+        <meta name='title' content='TEDxNITRourkela | Partners' />
+        <link rel='canonical' href='https://tedxnitrourkela.com/partners' />
+
+        {PARTNER.ONE.partners.map(({ hrefTitle }) => (
+          <meta
+            name='description'
+            content={`TEDxNITRourkela | Partner | ${hrefTitle}`}
+            key={hrefTitle}
+          />
+        ))}
+        {PARTNER.TWO.partners.map(({ hrefTitle }) => (
+          <meta
+            name='description'
+            content={`TEDxNITRourkela | Partner | ${hrefTitle}`}
+            key={hrefTitle}
+          />
+        ))}
+
+        {PARTNER.THREE.partners.map(({ hrefTitle }) => (
+          <meta
+            name='description'
+            content={`TEDxNITRourkela | Partner | ${hrefTitle}`}
+            key={hrefTitle}
+          />
+        ))}
+
+        {PARTNER.FOUR.partners.map(({ hrefTitle }) => (
+          <meta
+            name='description'
+            content={`TEDxNITRourkela | Partner | ${hrefTitle}`}
+            key={hrefTitle}
+          />
+        ))}
+
+        {PARTNER.FIVE.partners.map(({ hrefTitle }) => (
+          <meta
+            name='description'
+            content={`TEDxNITRourkela | Partner | ${hrefTitle}`}
+            key={hrefTitle}
+          />
+        ))}
+      </Helmet>
+
+      <Container style={{ marginTop: '150px' }}>
+        <Partners DATA={PARTNER.ONE} main />
+        <Partners DATA={PARTNER.TWO} />
+        <Partners DATA={PARTNER.THREE} />
+        <Partners DATA={PARTNER.FOUR} />
+        <Partners DATA={PARTNER.FIVE} />
+
+        <ContentBanner DATA={PARTNER.CONTENT} wrapContainer />
+      </Container>
+    </>
   );
 }
 
 export default Partner;
-
-const useStyles = makeStyles((theme) => ({
-  image: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-  },
-  partnerContainer: {
-    margin: '20px 0px',
-    flex: 1,
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imgContainer: {
-    height: 'auto',
-    margin: '10px 10px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sponsor: {
-    opacity: (main) => (main ? 0.9 : 0.5),
-    width: '90%',
-    height: 'auto',
-  },
-  titleContainer: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  title: {
-    marginBottom: '10px',
-    fontFamily: 'Zilla Slab',
-    fontWeight: 700,
-    textAlign: 'center',
-    color: theme.palette.common.white,
-  },
-  horizontalLine: {
-    width: '20%',
-    height: '1px',
-    backgroundColor: '#ffffff',
-    opacity: 0.3,
-  },
-  navigator: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignContent: 'center',
-    color: '#ffffff',
-  },
-}));
