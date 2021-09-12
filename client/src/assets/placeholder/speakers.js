3a2a3790e48e2c7c45fe74c9413f2521a5f2e8e4
Ritesh Patil <riteshsp2000@gmail.com>
2021-09-12 19:03:13 +0530

    add next speaker

--- client/src/assets/placeholder/speakers.js
+++ client/src/assets/placeholder/speakers.js
@@ -47,305 +47,42 @@ export const LIVEGUESTS = [
       },
     ],
   },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Professor Anup Nandy, a researcher in AI & ML area and professor at ',
-  //     { href: 'https://nitrkl.ac.in/', content: 'NIT Rourkela' },
-  //     ', was one of the few Indian scientists selected to present his research in the momentous 5th BRICS Young Scientist Conclave 2020.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.TWO,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/anup-nandy-14032a178/',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://www.nitrkl.ac.in/FProfile.aspx?nandya',
-  //       link_type: 'fas fa-graduation-cap',
-  //       link_name: 'nit-rourkela',
-  //       aria_label: 'NIT Rourkela Profile',
-  //     },
-  //     {
-  //       link: 'https://scholar.google.co.in/citations?user=9bnp0PQAAAAJ&hl=en',
-  //       link_type: 'fas fa-book',
-  //       link_name: 'google-scholar',
-  //       aria_label: 'Google Scholar Profile',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Lisa Russell, MPH, is an ',
-  //     { href: 'https://emmys.com', content: 'Emmy-winning' },
-  //     ' filmmaker and art curator. She is a two-time ',
-  //     {
-  //       href: 'https://www.ted.com/about/programs-initiatives/tedx-program',
-  //       content: 'TEDx',
-  //     },
-  //     ' speaker, Fulbright Specialist and an accomplished presenter along with many other accolades.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.THREE,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/lisarussellfilms/',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://twitter.com/lisarusselfilm',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://www.instagram.com/lisarussellfilm/',
-  //       link_type: 'fab fa-instagram',
-  //       link_name: 'instagram',
-  //       aria_label: 'Instagram',
-  //     },
-  //     {
-  //       link: 'http://lisa-russell-films.squarespace.com/',
-  //       link_type: 'fab fa-squarespace',
-  //       link_name: 'squarespace',
-  //       aria_label: 'Website',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Pranav Khaitan is a senior engineering lead at Google AI where he led various R&D projects like Chrome, Youtube. He was also a part of the ',
-  //     { href: 'https://www.nobelprize.org/', content: 'Nobel Peace Prize' },
-  //     ' winning team in 2020. He has also been working to solve humanitarian aid problems in partnership with the ',
-  //     { href: 'https://www.wfp.org/', content: 'UN' },
-  //     ' and the ',
-  //     { href: 'https://www.worldbank.org/en/home', content: 'World Bank.' },
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.FOUR,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/pranavkhaitan/',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://twitter.com/pranavkhaitan',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://research.google/people/106395/',
-  //       link_type: 'fas fa-book',
-  //       link_name: 'google-scholar',
-  //       aria_label: 'Google Scholar Profile',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Jeremy has been maintaining the ',
-  //     { href: 'https://covid19india.org', content: 'covid19india.org' },
-  //     ' website since its initial push to production, giving access to centralized, reliable data presented with clarity and good faith.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.FIVE,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/jeremy-philemon-363129121',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://twitter.com/jeremyphilemon?s=09',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://github.com/jeremyphilemon',
-  //       link_type: 'fab fa-github',
-  //       link_name: 'github',
-  //       aria_label: 'Github',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Co-founder at ',
-  //     { href: 'https://finshots.in/', content: 'Finshots' },
-  //     ', a popular and emerging finance bulletin, he is an alumnus of ',
-  //     { href: 'https://home.iitd.ac.in/', content: 'IIT Delhi' },
-  //     ' and one of the few people who truly understand the meaning of building revolutionary services and technology.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.SIX,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://in.linkedin.com/in/lokeshgurram',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://twitter.com/lokesh_gurram',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://www.quora.com/profile/Lokesh-Gurram',
-  //       link_type: 'fab fa-quora',
-  //       link_name: 'quora',
-  //       aria_label: 'Quora',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Kabir Cafe is a Mumbai-based folk fusion band known for taking the voice of the saint Kabir Das across the country through their compositions.  In an era where music is losing its credibility, Kabir Cafe has made sure that they present Kabir’s poetry musically without compromising its beauty. Their song ',
-  //     {
-  //       href:
-  //         'https://open.spotify.com/album/4qsUOnusaOfaWLEVye5ekB?highlight=spotify:track:18dts4OqWOQTGFlMTGodkk',
-  //       content: '‘Mat Kar Maya ko Ahankar’',
-  //     },
-  //     ' for instance, was featured in the hit series, ',
-  //     {
-  //       href:
-  //         'https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta--1700000292',
-  //       content: '“Scam 1992’',
-  //     },
-  //     ', and was widely critically acclaimed.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.SEVEN,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://twitter.com/KabirCafe',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://www.instagram.com/kabircafelive/',
-  //       link_type: 'fab fa-instagram',
-  //       link_name: 'instagram',
-  //       aria_label: 'Instagram',
-  //     },
-  //     {
-  //       link: 'https://open.spotify.com/artist/2AWF4fUAstzXyJd01JTuS3',
-  //       link_type: 'fab fa-spotify',
-  //       link_name: 'spotify',
-  //       aria_label: 'Spotify',
-  //     },
-  //     {
-  //       link: 'https://www.youtube.com/channel/UCh4HyUdUyM7Na3z_Ls-LmnA',
-  //       link_type: 'fab fa-youtube',
-  //       link_name: 'youtube',
-  //       aria_label: 'Youtube',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Mr. Biswas is an alumnus of IITG and co-founder of numerous revolutionary companies, Pointo being one of them. Pointo provides an app-based e-rickshaw booking facility. Rodo, the brainchild of Mr. Biswas, is India’s FIRST dock-less public e-bicycle sharing service that aims for a cleaner, greener, hassle-free environment',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.EIGHT,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/riki-biswas-488ba2a0/',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://www.instagram.com/riki.biswas.iitg/?hl=en',
-  //       link_type: 'fab fa-instagram',
-  //       link_name: 'instagram',
-  //       aria_label: 'Instagram',
-  //     },
-  //     {
-  //       link: 'https://www.facebook.com/riki.biswas.52',
-  //       link_type: 'fab fa-facebook-f',
-  //       link_name: 'facebook',
-  //       aria_label: 'Facebook',
-  //     },
-  //   ],
-  // },
-  // {
-  //   name: '',
-  //   description: [
-  //     'Mr. Patra is an ',
-  //     { href: 'https://ximb.edu.in/', content: 'NIT Rourkela' },
-  //     ' alumnus and did his Masters from ',
-  //     { href: '', content: 'XIMB' },
-  //     ,
-  //     '. After years in corporate, he turned his life and started his filming company Wild Tiger Productions. He has been featured in press releases of Adobe Spark, ',
-  //     {
-  //       href:
-  //         'https://www.newindianexpress.com/education/edex/2014/jun/30/Preserving-Wildlife-his-Priority-630148.html',
-  //       content: 'The New Indian Express',
-  //     },
-  //     ' to name a few.',
-  //   ],
-  //   shortDescription: '',
-  //   image: SPEAKERS.SILHOUTTE.NINE,
-  //   isLongCard: false,
-  //   links: [
-  //     {
-  //       link: 'https://www.linkedin.com/in/sapatra/',
-  //       link_type: 'fab fa-linkedin-in',
-  //       link_name: 'linkedin',
-  //       aria_label: 'LinkedIn',
-  //     },
-  //     {
-  //       link: 'https://www.instagram.com/indiawilds/',
-  //       link_type: 'fab fa-instagram',
-  //       link_name: 'instagram',
-  //       aria_label: 'Instagram',
-  //     },
-  //     {
-  //       link: 'https://www.facebook.com/riki.biswas.52',
-  //       link_type: 'fab fa-facebook-f',
-  //       link_name: 'facebook',
-  //       aria_label: 'Facebook',
-  //     },
-  //     {
-  //       link: 'https://twitter.com/indiawilds',
-  //       link_type: 'fab fa-twitter',
-  //       link_name: 'twitter',
-  //       aria_label: 'Twitter',
-  //     },
-  //     {
-  //       link: 'https://www.indiawilds.com/',
-  //       link_type: 'fab fa-squarespace',
-  //       link_name: 'squarespace',
-  //       aria_label: 'Website',
-  //     },
-  //   ],
-  // },
+  {
+    name: 'Adam Grant',
+    description: [
+      'Adam Grant is a professor at the ',
+      {
+        href: 'https://mgmt.wharton.upenn.edu/profile/grantad/',
+        content: 'Wharton School of the University of Pennsylvania',
+      },
+      ', specializing in organizational psychology. Apart from being a psychologist, ​He is the #1 New York Times bestselling author of 5 books: Think Again, Give and Take, Originals, Option B, and Power Moves. ',
+    ],
+    shortDescription:
+      'Organizational Psychologist | New York Times bestselling author',
+    image: SPEAKERS.LIVESPEAKERS.TWO,
+    isLongCard: false,
+    links: [
+      {
+        link: 'https://mgmt.wharton.upenn.edu/profile/grantad/',
+        link_type: 'fas fa-university',
+        link_name: 'University',
+        aria_label: 'University',
+      },
+      {
+        link:
+          'https://twitter.com/AdamMGrant?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+        link_type: 'fab fa-twitter-square',
+        link_name: 'twitter',
+        aria_label: 'Twitter',
+      },
+      {
+        link: 'https://www.linkedin.com/in/adammgrant/',
+        link_type: 'fab fa-linkedin',
+        link_name: 'Linkedin',
+        aria_label: 'Linkedin',
+      },
+    ],
+  },
 ];
 
 export const GUESTS = [
